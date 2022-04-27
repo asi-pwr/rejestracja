3108fde7502c022a158950a883dd390f887faaa4
Michał Zając <michal.zajac@gmail.com>
2022-04-27 21:29:53 +0200

    Update README

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-docker build -t rejestracja-asi:latest .
-docker run -it --rm -v "./data:/usr/src/app/data" -p "127.0.0.1:9292:9292" rejestracja-asi:latest
+
+# You can build an image yourself by running:
+# docker build -t rejestracja-asi:latest .
+# then you can start it by running
+# docker run -it --rm -v "./data:/usr/src/app/data" -p "127.0.0.1:9292:9292" rejestracja-asi:latest
+
+docker run -it --rm -v "./data:/usr/src/app/data" -p "127.0.0.1:9292:9292" ghcr.io/asi-pwr/rejestracja:latest
